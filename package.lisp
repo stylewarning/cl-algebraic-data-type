504010ab7ce62237760b9b283c4ce8cae4296b92
Robert Smith <robert@stylewarning.com>
2014-10-09 19:04:17 -0700

    Add ALGEBRAIC-DATA-VALUE-P to test if something is from an ADT.
    Also add some documentation.

--- package.lisp
+++ package.lisp
@@ -1,15 +1,18 @@
 ;;;; package.lisp
+;;;;
 ;;;; Copyright (c) 2012-2014 Robert Smith
 
 (defpackage #:cl-algebraic-data-type
+  (:documentation "A package for defining algebraic data types.")
   (:use #:cl)
   (:nicknames #:adt)
   (:export
-   #:defdata
-   #:set-data
-   #:with-data
-   #:match
-   #:algebraic-data-type
-   #:algebraic-data-type-p
-   #:get-constructors))
-
+   #:defdata                            ; MACRO
+   #:set-data                           ; MACRO
+   #:with-data                          ; MACRO
+   #:match                              ; MACRO
+   #:algebraic-data-type                ; TYPE
+   #:algebraic-data-type-p              ; FUNCTION (PREDICATE)
+   #:algebraic-data-value-p             ; FUNCTION (PREDICATE)
+   #:get-constructors                   ; FUNCTION
+   ))
